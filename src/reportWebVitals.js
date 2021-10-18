d383269650ff22d80a104726dab828a3dca5ff95
Dico Diaz <dicodiaz@gmail.com>
2021-10-18 17:46:30 -0500

    Remove eslint rule ignore and fix linter error

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,8 @@
-/* eslint-disable object-curly-newline */
-
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({
+      getCLS, getFID, getFCP, getLCP, getTTFB,
+    }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
