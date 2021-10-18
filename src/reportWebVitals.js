0431fa778fbf5ee98c397436c3a8f3d645e5236c
Dico Diaz <dicodiaz@gmail.com>
2021-10-18 16:57:51 -0500

    Update README, setup linters and fix linter errors

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,6 @@
-const reportWebVitals = onPerfEntry => {
+/* eslint-disable object-curly-newline */
+
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
