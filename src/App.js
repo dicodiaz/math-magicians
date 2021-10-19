5d65f673280730b21247636e31cb110355b770e4
Dico Diaz <dicodiaz@gmail.com>
2021-10-19 14:27:22 -0500

    Refactor App component as class-based

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <main className="bg-dark min-vh-100 d-flex flex-column justify-content-center">
-    <div className="container-md">
-      <div className="row mx-0 justify-content-center">
-        <div className="col-md-9">
-          <Calculator />
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+  render() {
+    return (
+      <main className="bg-dark min-vh-100 d-flex flex-column justify-content-center">
+        <div className="container-md">
+          <div className="row mx-0 justify-content-center">
+            <div className="col-md-9">
+              <Calculator />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </main>
-);
-
-export default App;
+      </main>
+    );
+  }
+}
