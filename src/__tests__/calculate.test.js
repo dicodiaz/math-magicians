979e7bc9e174aaa3f5913dc2ec8000ad8abb1321
AlexHamn <agalexandro16@gmail.com>
2021-10-28 11:30:44 -0500

    Finish button testing

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -56,4 +56,49 @@ describe('testing calculate logic', () => {
     Object.assign(obj, calculate(obj, '8'));
     expect(obj).toEqual({ total: '579', next: '78', operation: '−' });
   });
+
+  test('testing the "9" button', () => {
+    Object.assign(obj, calculate(obj, '9'));
+    expect(obj).toEqual({ total: '579', next: '789', operation: '−' });
+  });
+
+  test('testing the "×" button', () => {
+    Object.assign(obj, calculate(obj, '×'));
+    expect(obj).toEqual({ total: '-210', next: null, operation: '×' });
+  });
+
+  test('testing the "+/-" button', () => {
+    Object.assign(obj, calculate(obj, '+/-'));
+    expect(obj).toEqual({ total: '210', next: null, operation: '×' });
+  });
+
+  test('testing the "%" button', () => {
+    Object.assign(obj, calculate(obj, '%'));
+    expect(obj).toEqual({ total: '210', next: null, operation: '%' });
+  });
+
+  test('testing the "÷" button', () => {
+    Object.assign(obj, calculate(obj, '÷'));
+    expect(obj).toEqual({ total: '210', next: null, operation: '÷' });
+  });
+
+  test('testing the "0" button', () => {
+    Object.assign(obj, calculate(obj, '0'));
+    expect(obj).toEqual({ total: '210', next: '0', operation: '÷' });
+  });
+
+  test('testing the "." button', () => {
+    Object.assign(obj, calculate(obj, '.'));
+    expect(obj).toEqual({ total: '210', next: '0.', operation: '÷' });
+  });
+
+  test('testing the "=" button', () => {
+    Object.assign(obj, calculate(obj, '='));
+    expect(obj).toEqual({ total: 'Can\'t divide by 0.', next: null, operation:null });
+  });
+
+  test('testing the "AC" button', () => {
+    Object.assign(obj, calculate(obj, 'AC'));
+    expect(obj).toEqual({ total: '0', next: null, operation: null });
+  });
 });
