eb6d07d45ff7aad397f444de096e6ec152b2885b
AlexHamn <agalexandro16@gmail.com>
2021-10-28 12:23:35 -0500

    Finish component testing

--- src/__tests__/components.test.js
+++ src/__tests__/components.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Button from '../components/Button';
 import Calculator from '../components/Calculator';
+import Clock from '../components/Clock';
+import Header from '../components/Header';
+
 
 describe('testing components', () => {
   test('testing Button.js', () => {
@@ -13,6 +17,16 @@ describe('testing components', () => {
 
   test('testing Calculator.js', () => {
     const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('testing Clock.js', () => {
+    const tree = renderer.create(<Clock updateEveryNSeconds={2}/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('testing Header.js', () => {
+    const tree = renderer.create(<Router> <Header /> </Router>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
