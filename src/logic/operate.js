cfb08617e67aee2da1e15a2f80e2217c755313d8
Dico Diaz <dicodiaz@gmail.com>
2023-07-26 13:18:15 -0500

    Refactor logic and data

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,26 +3,25 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   const one = Big(numberOne);
   const two = Big(numberTwo);
-  if (operation === '+') {
-    return one.plus(two).toString();
+
+  switch (operation) {
+    case '+':
+      return one.plus(two).toString();
+    case '-':
+      return one.minus(two).toString();
+    case 'x':
+      return one.times(two).toString();
+    case '/':
+      try {
+        return one.div(two).toString();
+      } catch (err) {
+        return "Can't divide by 0";
+      }
+    case '%':
+      return one.mod(two).toString();
+    default:
+      throw Error(`Unknown operation '${operation}'`);
   }
-  if (operation === '-') {
-    return one.minus(two).toString();
-  }
-  if (operation === 'x') {
-    return one.times(two).toString();
-  }
-  if (operation === '/') {
-    try {
-      return one.div(two).toString();
-    } catch (err) {
-      return "Can't divide by 0";
-    }
-  }
-  if (operation === '%') {
-    return one.mod(two).toString();
-  }
-  throw Error(`Unknown operation '${operation}'`);
 };
 
 export default operate;
