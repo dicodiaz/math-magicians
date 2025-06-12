1b518d1bf692e122361fc929346f5406bc2ded6b
Dico Diaz <dicodiaz@gmail.com>
2025-06-12 14:28:06 -0500

    Fix Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,45 @@
-import { AiOutlineMenu } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import links from '../data/links';
 
-const Header = () => (
-  <nav className="navbar navbar-expand-md navbar-light bg-secondary fixed-top">
-    <div className="container-fluid">
-      <span className="navbar-brand text-white">Math Magicians</span>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <AiOutlineMenu style={{ color: 'white', fontSize: '25px' }} />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="v-line d-none d-md-block" />
-          <li className="nav-item">
-            <Link to="/calculate">Calculator</Link>
-          </li>
-          <li className="v-line d-none d-md-block" />
-          <li className="nav-item">
-            <Link to="/quote">Quote</Link>
-          </li>
-        </ul>
+const Header = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-secondary fixed-top container-fluid">
+      <div className="container-fluid">
+        <span className="navbar-brand text-white">Math Magicians</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav ms-auto">
+            {links.map(({ id, to, text }) => {
+              const classNames = ['nav-item', 'nav-link'];
+              if (to === pathname) {
+                classNames.push('active');
+              }
+
+              return (
+                <Link key={id} className={classNames.join(' ')} to={to}>
+                  <div data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    {text}
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Header;
