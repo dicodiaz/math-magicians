693f8ed3b4a02ab46787e06d33ae1e7a5230d83d
Dico Diaz <dicodiaz@gmail.com>
2025-06-12 14:27:46 -0500

    Refactor all pages

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -7,18 +7,23 @@ const operate = (numberOne, numberTwo, operation) => {
   switch (operation) {
     case '+':
       return one.plus(two).toString();
+
     case '-':
       return one.minus(two).toString();
+
     case 'x':
       return one.times(two).toString();
+
     case '/':
       try {
         return one.div(two).toString();
       } catch (err) {
         return "Can't divide by 0";
       }
+
     case '%':
       return one.mod(two).toString();
+
     default:
       throw Error(`Unknown operation '${operation}'`);
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import welcomeMessages from '../data/welcomeMessages';
 
 const Home = () => (
-  <div className="text-white pt-5">
-    <h1 className="display-1 mb-3">Welcome to our page!</h1>
+  <div className="text-white pt-5 mt-4 mt-sm-0">
+    <h1 className="display-1 mb-3">Welcome to our Math Playground!</h1>
     {welcomeMessages.map(({ id, text }) => (
       <p key={id} className="lead">
         {text}
