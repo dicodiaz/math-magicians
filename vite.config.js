8f49d9be8531b17792b73b71c2274c4df22665ad
Dico Diaz <dicodiaz@gmail.com>
2025-06-12 14:56:39 -0500

    Implement Vite Plugin Federation

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import federation from '@originjs/vite-plugin-federation';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'mathMagicians',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Calculator': './src/components/Calculator',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
 });
